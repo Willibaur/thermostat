1a11170fc47c88b551d766358e31b083212e78b7
willibaur <williambautista@gmail.com>
2016-01-06 14:44:36 +0000

    Increase and decrease tests added

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,28 @@
+describe('Thermostat', function() {
+  var thermostat;
+});
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', function() {
+    expect(thermostat.getCurrentTemp()).toEqual(20);
+  });
+
+  it('increases temperature by 1 degree', function() {
+    thermostat.increase();
+    expect(thermostat.getCurrentTemp()).toEqual(21);
+  });
+
+  it('dreceases temperature by 1 degree', function() {
+    thermostat.decrease();
+    expect(thermostat.getCurrentTemp()).toEqual(19);
+  });
+
+  it('prevents to go below 10 degrees', function() {
+    do {
+      thermostat.decrease();
+    } while (thermostat.getCurrentTemp() >= 10);
+    expect(function() { thermostat.decrease(); }).toThrowError('Temperature cannot below 10');
+  });
---
+++ src/thermostat.js
@@ -0,0 +1,22 @@
+function Thermostat() {
+  this.temperature = 20;
+}
+
+ var MINTEMP = 10;
+
+Thermostat.prototype.getCurrentTemp = function () {
+  return this.temperature;
+};
+
+Thermostat.prototype.increase = function () {
+  this.temperature += 1;
+};
+
+Thermostat.prototype.decrease = function () {
+  if (this.temperature < 10) {
+    throw new Error ('Temperature cannot below 10');
+  }
+  else {
+    this.temperature -= 1;
+  }
+};
