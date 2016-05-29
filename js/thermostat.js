eae232b29d4c01fe5cb2951218d46dc0e9cda9c2
William Bautista <williambautista@gmail.com>
2016-05-29 19:35:05 +0100

    jshint added and tests suite refactor WIP

---
+++ js/thermostat.js
@@ -0,0 +1,71 @@
+function Thermostat() {
+  this.temp = DEFAULT_TEMP;
+  this.MIN_TEMP = 10;
+  this.powerSave = true;
+  this.maxTemp_PSM_ON = 25;
+  this.maxTemp_PSM_OFF = 32;
+  this.MED_ENERGY_USAGE_LIMIT = 18;
+}
+
+const DEFAULT_TEMP = 20;
+
+
+Thermostat.prototype.getTemp = function() {
+  return this.temp;
+};
+
+Thermostat.prototype.isMinTemp = function(){
+  return this.temp === this.MIN_TEMP;
+};
+
+Thermostat.prototype.isMaxTemp = function(){
+  if(this.isPowerSaveOn() === false){
+    return this.temp === this.maxTemp_PSM_OFF;
+  }
+  return this.temp === this.maxTemp_PSM_ON;
+};
+
+Thermostat.prototype.increaseTemp = function() {
+  if (this.isMaxTemp()){
+    return;
+  }
+  this.temp += 1;
+};
+
+Thermostat.prototype.decreseTemp = function(){
+  if (this.isMinTemp()) {
+    return;
+  }
+  this.temp -= 1;
+};
+
+Thermostat.prototype.isPowerSaveOn = function() {
+  return this.powerSave === true;
+};
+
+Thermostat.prototype.turnPowerSaveOff = function () {
+  this.powerSave = false;
+  this.maxTemp = this.maxTemp_PSM_OFF;
+};
+
+Thermostat.prototype.turnPowerSaveOn = function () {
+  this.powerSave = true;
+  this.maxTemp = this.maxTemp_PSM_ON;
+  if (this.temp > this.maxTemp_PSM_ON) {
+    this.temp = this.maxTemp;
+  }
+};
+
+Thermostat.prototype.reset = function() {
+  this.temp = DEFAULT_TEMP;
+};
+
+Thermostat.prototype.energyUsage = function () {
+  if (this.temp < this.MED_ENERGY_USAGE_LIMIT) {
+    return 'low-usage';
+  }
+  if (this.temp >= this.MED_ENERGY_USAGE_LIMIT && this.temp <= this.maxTemp_PSM_ON) {
+    return 'medium-usage';
+  }
+  return 'high-usage';
+};
--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,98 +1,96 @@
 describe('Thermostat', function() {
+
   var thermostat;
-});
 
   beforeEach(function() {
     thermostat = new Thermostat();
   });
 
-  describe('sets up default settings', function() {
-    it('initial temperature to 20', function() {
-      expect(thermostat.getCurrentTemp()).toEqual(20);
+  describe('sets up default settings where', function() {
+    it('initial temperature is equals to 20', function() {
+      expect(thermostat.getTemp()).toEqual(20);
     });
 
-    it('power saving mode is on', function() {
-      do {
-        thermostat.increase();
-      } while (thermostat.getCurrentTemp() <= 25);
-      expect(function() { thermostat.increase(); }).toThrowError('Temperature cannot be above 25');
+    it('power saving mode is ON', function() {
+      expect(thermostat.isPowerSaveOn()).toBe(true);
     });
   });
 
-  describe('changes temperature by', function() {
-    it('+1 degree', function() {
-      thermostat.increase();
-      expect(thermostat.getCurrentTemp()).toEqual(21);
-    });
-
-    it('-1 degree', function() {
-      thermostat.decrease();
-      expect(thermostat.getCurrentTemp()).toEqual(19);
-    });
-  });
-
-  describe('when power saving is', function() {
-    it('on, sets maximum temperature to 25 degrees', function() {
-      do {
-        thermostat.increase();
-      } while (thermostat.getCurrentTemp() <= 25);
-      expect(function() { thermostat.increase(); }).toThrowError('Temperature cannot be above 25');
-    });
-
-    it('off, sets maximum temperature to 32 degrees', function() {
-      thermostat.powerSavingMode();
-      do {
-        thermostat.increase();
-      } while (thermostat.getCurrentTemp() <= 32);
-      expect(function() { thermostat.increase(); }).toThrowError('Temperature cannot be above 32');
-    });
-  });
-
-  describe('temperature sensor', function() {
-    it('cannot be below 10 degrees', function() {
-      do {
-        thermostat.decrease();
-      } while (thermostat.getCurrentTemp() >= 10);
-      expect(function() { thermostat.decrease(); }).toThrowError('Temperature cannot below 10');
-    });
-
-    describe('can be reset to 20 degrees if', function() {
-      it('current temperature is < 20 degrees', function() {
-        do {
-          thermostat.decrease();
-        } while (thermostat.getCurrentTemp() >= 10);
-        thermostat.resetTemp();
-        expect(thermostat.getCurrentTemp()).toEqual(20);
-      });
-
-      it('current temperature is > 20 degrees', function() {
-        thermostat.powerSavingMode();
-        do {
-          thermostat.increase();
-        } while (thermostat.getCurrentTemp() >= 32);
-        thermostat.resetTemp();
-        expect(thermostat.getCurrentTemp()).toEqual(20);
-      });
-    });
-
-    describe('displays color', function() {
-      it('green when temperature < 18 degrees', function() {
-        do {
-          thermostat.decrease();
-        } while (thermostat.getCurrentTemp() > 17);
-        expect(thermostat.getColorDisplay()).toEqual('green');
-      });
-
-      it('yellow when temperature between 18  and 25 degrees', function() {
-        expect(thermostat.getColorDisplay()).toEqual('yellow');
-      });
-
-      it('red when temperature > 25 degrees', function() {
-        thermostat.powerSavingMode();
-        do {
-          thermostat.increase();
-        } while (thermostat.getCurrentTemp() <= 25);
-        expect(thermostat.getColorDisplay()).toEqual('red');
-      });
-    });
-  });
+  // describe('changes temperature by', function() {
+  //   it('+1 degree', function() {
+  //     thermostat.increaseTemp();
+  //     expect(thermostat.getTemp()).toEqual(21);
+  //   });
+  //
+  //   it('-1 degree', function() {
+  //     thermostat.decrease();
+  //     expect(thermostat.getTemp()).toEqual(19);
+  //   });
+  // });
+  //
+  // describe('when power saving is', function() {
+  //   it('on, sets maximum temperature to 25 degrees', function() {
+  //     do {
+  //       thermostat.increaseTemp();
+  //     } while (thermostat.getTemp() <= 25);
+  //     expect(function() { thermostat.increaseTemp(); }).toThrowError('Temperature cannot be above 25');
+  //   });
+  //
+  //   it('off, sets maximum temperature to 32 degrees', function() {
+  //     thermostat.powerSavingMode();
+  //     do {
+  //       thermostat.increaseTemp();
+  //     } while (thermostat.getTemp() <= 32);
+  //     expect(function() { thermostat.increaseTemp(); }).toThrowError('Temperature cannot be above 32');
+  //   });
+  // });
+  //
+  // describe('temperature sensor', function() {
+  //   it('cannot be below 10 degrees', function() {
+  //     do {
+  //       thermostat.decrease();
+  //     } while (thermostat.getTemp() >= 10);
+  //     expect(function() { thermostat.decrease(); }).toThrowError('Temperature cannot below 10');
+  //   });
+  //
+  //   describe('can be reset to 20 degrees if', function() {
+  //     it('current temperature is < 20 degrees', function() {
+  //       do {
+  //         thermostat.decrease();
+  //       } while (thermostat.getTemp() >= 10);
+  //       thermostat.resetTemp();
+  //       expect(thermostat.getTemp()).toEqual(20);
+  //     });
+  //
+  //     it('current temperature is > 20 degrees', function() {
+  //       thermostat.powerSavingMode();
+  //       do {
+  //         thermostat.increaseTemp();
+  //       } while (thermostat.getTemp() >= 32);
+  //       thermostat.resetTemp();
+  //       expect(thermostat.getTemp()).toEqual(20);
+  //     });
+  //   });
+  //
+  //   describe('displays color', function() {
+  //     it('green when temperature < 18 degrees', function() {
+  //       do {
+  //         thermostat.decrease();
+  //       } while (thermostat.getTemp() > 17);
+  //       expect(thermostat.getColorDisplay()).toEqual('green');
+  //     });
+  //
+  //     it('yellow when temperature between 18  and 25 degrees', function() {
+  //       expect(thermostat.getColorDisplay()).toEqual('yellow');
+  //     });
+  //
+  //     it('red when temperature > 25 degrees', function() {
+  //       thermostat.powerSavingMode();
+  //       do {
+  //         thermostat.increaseTemp();
+  //       } while (thermostat.getTemp() <= 25);
+  //       expect(thermostat.getColorDisplay()).toEqual('red');
+  //     });
+  //   });
+  // });
+});
